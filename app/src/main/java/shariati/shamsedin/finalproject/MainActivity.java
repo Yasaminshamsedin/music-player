2cbb468a26b650d2d94b43e0ea850b5e107c9012
yasaminshamsedin <y.shamsedin@gmail.com>
2023-01-12 02:15:09 +0330

    home_intents

--- app/src/main/java/shariati/shamsedin/finalproject/MainActivity.java
+++ app/src/main/java/shariati/shamsedin/finalproject/MainActivity.java
@@ -1,14 +1,61 @@
 package shariati.shamsedin.finalproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    ImageView btnsearch;
+    LinearLayout btnneww;
+    LinearLayout btnbest;
+    LinearLayout btnday;
+    LinearLayout btnweek;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        btnsearch= findViewById(R.id.btnsearch);
+        btnsearch.setOnClickListener(view -> {
+            Intent a =new Intent(MainActivity.this,Search.class);
+            startActivity(a);
+        });
+        btnneww= findViewById(R.id.btnneww);
+        btnneww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent b =new Intent(MainActivity.this,Neww.class);
+                startActivity(b);
+            }
+        });
+        btnbest= findViewById(R.id.btnbest);
+        btnbest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent c =new Intent(MainActivity.this,Best.class);
+                startActivity(c);
+            }
+        });
+        btnday= findViewById(R.id.btnday);
+        btnbest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent d =new Intent(MainActivity.this,Day.class);
+                startActivity(d);
+            }
+        });
+        btnweek= findViewById(R.id.btnweek);
+        btnweek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent e =new Intent(MainActivity.this,Week.class);
+                startActivity(e);
+            }
+        });
     }
 }
---
+++ app/src/main/java/shariati/shamsedin/finalproject/Neww.java
@@ -0,0 +1,14 @@
+package shariati.shamsedin.finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Neww extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neww);
+    }
+}
