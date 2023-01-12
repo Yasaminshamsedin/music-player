8962fdd38211a2a8a756c4759c10566c0e4e1a41
yasaminshamsedin <y.shamsedin@gmail.com>
2023-01-13 00:54:46 +0330

    connection_internet

--- app/src/main/java/shariati/shamsedin/finalproject/Search.java
+++ app/src/main/java/shariati/shamsedin/finalproject/Search.java
@@ -1,10 +1,16 @@
 package shariati.shamsedin.finalproject;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Search extends AppCompatActivity {
+    EditText edSearch;
+
+    RecyclerView recyclerViewSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/shariati/shamsedin/finalproject/splash.java
+++ app/src/main/java/shariati/shamsedin/finalproject/splash.java
@@ -1,10 +1,16 @@
 package shariati.shamsedin.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
 
@@ -12,13 +18,51 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i=new Intent(splash.this,MainActivity.class);
-                startActivity(i);
-                finish();
-            }
+        checkConnection();
+    }
+    void Show(){
+
+        new Handler().postDelayed(() -> {
+            Intent i=new Intent(splash.this,MainActivity.class);
+            startActivity(i);
+            finish();
         },3000);
+    }
+    protected boolean internet() {
+        ConnectivityManager ConnectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo NetworkInfo = ConnectivityManager.getActiveNetworkInfo();
+        return NetworkInfo != null && NetworkInfo.isConnectedOrConnecting();
+    }
+
+    public void checkConnection() {
+        if (internet())
+        {
+            Show();
+        }
+        else
+        {
+            AlertDialog.Builder Dialog = new AlertDialog.Builder(this);
+            View activity = LayoutInflater.from(this).inflate(R.layout.connection,null);
+            Dialog.setView(activity);
+            AlertDialog alertDialog = Dialog.create();
+
+            alertDialog.setCancelable(false);
+
+            View btnexsit = activity.findViewById(R.id.btnexsit);
+            View btnretry = activity.findViewById(R.id.btnrety);
+
+            btnretry.setOnClickListener(view -> {
+                alertDialog.dismiss();
+                checkConnection();
+            });
+            btnexsit.setOnClickListener(view -> {
+                alertDialog.dismiss();
+                finish();
+            });
+
+            alertDialog.show();
+        }
+
     }
 }
